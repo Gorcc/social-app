ecdb4fa333820d8b441a95bcebad1b825a691819
eren5515 <eren1256@gmail.com>
2023-12-20 00:38:07 +0300

    feat:added a demo account in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,39 @@
-"use client"
-import Link from 'next/link'
-import Messages from './messages'
-import "../../app/styles/base.css"
-
+"use client";
+import Link from "next/link";
+import Messages from "./messages";
+import "../../app/styles/base.css";
 
 export default function Login() {
-
   return (
     <div className="min-h-screen flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-     
-      
-
       <form
         className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
         action="/auth/sign-in"
         method="post"
       >
-        
         <label className="text-md" htmlFor="email">
           Email
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          className="rounded-md px-4 py-2 bg-inherit border"
           name="email"
           placeholder="you@example.com"
           required
         />
+        <p className="text-center mb-6">Demo User: socialappdemouser@outlook.com</p>
+
         <label className="text-md" htmlFor="password">
           Password
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          className="rounded-md px-4 py-2 bg-inherit border"
           type="password"
           name="password"
           placeholder="••••••••"
           required
         />
+                <p className="text-center mb-6">password: demo123</p>
+
         <button className="bg-green rounded px-4 py-2 text-white mb-2">
           Sign In
         </button>
@@ -48,5 +46,5 @@ export default function Login() {
         <Messages />
       </form>
     </div>
-  )
-  }
+  );
+}
